c2740fd93100472c977eb62a519f6c2c4c64feb4
MKraust <kraust137@gmail.com>
2022-04-08 19:51:22 +0300

    Set up compilation

--- packages/api-base/index.ts
+++ packages/api-base/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/api-wrapper'
-export * from './lib/client'
-export * from './lib/error-handler'
+export * from './src/api-wrapper'
+export * from './src/client'
+export * from './src/error-handler'
