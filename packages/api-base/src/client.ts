a1e808567603c659972053fd3a10d3855e0cfa65
MKraust <kraust137@gmail.com>
2022-04-19 14:14:22 +0300

    Revert FormData polyfill

--- packages/api-base/src/client.ts
+++ packages/api-base/src/client.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AxiosInstance, AxiosRequestConfig, AxiosResponse, ResponseType } from 'axios'
-import FormData from 'form-data'
 
 export type QueryParamsType = Record<string | number, unknown>
 
